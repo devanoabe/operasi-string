1d0d71113507409aaf87aa9320f3ecfa95cc31fd
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-16 15:25:58 +0700

    Coba method equals & equalsIgnoreCase

--- src/jobshhet11/Jobshhet11.java
+++ src/jobshhet11/Jobshhet11.java
@@ -23,6 +23,15 @@ public class Jobshhet11 {
        System.out.println("y adalah variable kosong : " + y.isEmpty());
        System.out.println("isi x sama dengan y : " + x.equals(y));
        
+       String z = "Operasi";
+       System.out.println("isi variable z : " + z);
+       System.out.println("isi x sama dengan z (Case Sensitive) : " + x.equals(z));
+       
+       String r = "Operasi";
+       System.out.println("isi variable r : " + r);
+       System.out.println("isi x sama dengan r (Case Sensitive) : " + x.equals(r));
+       System.out.println("isi x sama dengan r ( Not Case Sensitive) : " + x.equalsIgnoreCase(r));
+       
       
     }
     
