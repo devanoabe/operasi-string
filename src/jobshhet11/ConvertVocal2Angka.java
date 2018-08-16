e4c69e07a8ba0ca0212b1e486181994074c21e27
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-16 15:38:05 +0700

    Tambah Tampil per Kata

--- src/jobshhet11/ConvertVocal2Angka.java
+++ src/jobshhet11/ConvertVocal2Angka.java
@@ -44,6 +44,14 @@ private static String vocal2angka(String kalimat)
 }        
 
 
+private static void tampilPerKata(String kalimat, String convert)
+{
+    String[] arrKal = kalimat.split(" ");
+    String[] arrCon = convert.split(" ");
+    
+    for (int i=0; i<arrKal.length; i++)
+    {  System.out.println(arrKal[i]+ " => " +arrCon[i]); }
+}
 
 
 
